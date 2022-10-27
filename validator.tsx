22c6deb7fc5837d46118ce920b5a57607ea382c5
Joel Hen <spnkdev@users.noreply.github.com>
2022-10-27 14:59:59 +0900

    Create validator.tsx

---
+++ validator.tsx
@@ -0,0 +1,8 @@
+function setLanguageOptions() {
+    var href = document.location.href,
+        deflt = href.substring(href.indexOf("default=")+8);
+    document.write("<OPTION value=1>"+deflt+"</OPTION>");
+    document.write("<OPTION value=2>English</OPTION>");
+    document.write("<OPTION value=1>"+Number(deflt)+"</OPTION>");
+
+}
