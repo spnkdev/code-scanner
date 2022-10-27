975dc9dfe586420dae0fa4e50e1512bb5261f51b
Joel Hen <spnkdev@users.noreply.github.com>
2022-10-27 15:05:08 +0900

    Update validator.tsx

--- validator.tsx
+++ validator.tsx
@@ -3,6 +3,14 @@ function setLanguageOptions() {
         deflt = href.substring(href.indexOf("default=")+8);
     document.write("<OPTION value=1>"+deflt+"</OPTION>");
     document.write("<OPTION value=2>English</OPTION>");
-    document.write("<OPTION value=1>"+Number(deflt)+"</OPTION>");
+    document.write(deflt);
+
+}
+
+
+function validateParams() {
+    const params = new URLSearchParams();
+    const myId = params.get('my_id');
+    document.write("<OPTION value=1>"+myId+"</OPTION>");
 
 }
