a18a1624b9827bfa3a806995ee0489639d54e399
Joel Hen <spnkdev@users.noreply.github.com>
2023-03-14 15:16:41 +0900

    Update test.tsx

--- test.tsx
+++ test.tsx
@@ -3,4 +3,7 @@ function setLanguageOptions() {
         deflt = href.substring(href.indexOf("default=")+8);
     document.write("<OPTION value=1>"+deflt+"</OPTION>");
     document.write("<OPTION value=2>English</OPTION>");
+    document.write("<Input title='Email' context={ctx} stateName='email' validation={[
+                    { message: 'Invalid email', regex: '.+@.+\.[a-z]+' },
+                ]} />");
 }
