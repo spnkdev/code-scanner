e28b18dba098d8e05ded9992c705e189cc08dfad
Joel Hen <spnkdev@users.noreply.github.com>
2023-03-20 10:38:45 +0900

    Update test1.tsx

--- test1.tsx
+++ test1.tsx
@@ -1,1 +1,22 @@
-const idDocument = window.location.href.split('consistency-check/document/')[1].split('/quarter-section')[0];
+const app = require("express")(),
+      pg = require("pg"),
+      pool = new pg.Pool(config);
+
+app.get("search", function handler(req, res) {
+  // BAD: the category might have SQL special characters in it
+  const idDocument = window.location.href.split('consistency-check/document/')[1].split('/quarter-section')[0];
+  var query1 =
+    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
+    idDocument +
+    "' ORDER BY PRICE";
+  pool.query(query1, [], function(err, results) {
+    // process results
+  });
+
+  // GOOD: use parameters
+  var query2 =
+    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY=$1" + " ORDER BY PRICE";
+  pool.query(query2, [req.params.category], function(err, results) {
+    // process results
+  });
+});
